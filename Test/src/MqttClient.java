8604b890645c009056494f1d64e2dd4f20f958bd
Pleum-Jednipit <37322979+Pleum-Jednipit@users.noreply.github.com>
2020-03-05 19:47:22 +0700

    New

---
+++ Test/src/MqttClient.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+
+public class MqttClient extends Thread {
+    /*This variable is the id of the client, this id will be the MAC address of
+     *the using device*/
+    private String id;
+    /*Input stream with the server*/
+    private DataInputStream input;
+    /*Output stream with the server*/
+    private DataOutputStream output;
+    /*The socket of the client connected to the server*/
+    private Socket socket;
+    /*The topic on where the client want to be*/
+    private String topic;
+
+
+    /*Constructor of the client, this function will get the MAC address of the
+     *client, save the socket given in parameter, and open streams from this
+     *socket*/
+    public MqttClient(Socket Socket) {
+        id = getMacAddress();
+        socket = Socket;
+        if (socket != null) {
+            try {
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("stream connection failed");
+            }
+
+        }
+    }
+
+    /*Here is the Thread function, we try to get a command, if it's a subscribe
+     *command, a loop begin waiting for any publish in the apropiate topic,
+     *if it's a publish, we will search all clients register in the topic
+     *and send them the data associate with the command*/
+    public void run() {
+        String command = "";
+        String[] splitcommand = null;
+        String in = "";
+        String[] splitInput = null;
+        try {
+            command = input.readUTF();
+        } catch (IOException e) {
+            System.out.println("Impossible to get de command");
+        }
+        if (command != null) {
+            splitcommand = command.split(" ");
+        }
+        if ((!splitcommand[0].equals("publish") && !splitcommand[0].equals("subscribe")) ||
+                (splitcommand.length != 2 && splitcommand.length != 3)) {
+            System.out.println("(MqttClient) Invalid command");
+            System.out.println(command);
+            System.out.println(splitcommand[0]);
+        }
+        if (splitcommand[0].equals("subscribe")) {
+            topic = splitcommand[1];
+            System.out.println("Connected with subscriber :" + socket.getRemoteSocketAddress());
+            while (socket.isConnected()) ;
+        } else if (splitcommand[0].equals("publish")) {
+            ArrayList<MqttClient> clientList = MqttBroker.getClients();
+            System.out.println("Get message from publisher :" + socket.getRemoteSocketAddress());
+            System.out.println("message: " + splitcommand[2]);
+            for (MqttClient client : clientList) {
+                if (client.topic != null && client.topic.equals(splitcommand[1])) {
+                    try {
+                        System.out.println("Send to the subscriber " + id + ": " + splitcommand[2]);
+                        client.output.writeUTF(splitcommand[2]);
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+        try {
+            input.close();
+            output.close();
+        } catch (IOException e) {
+        }
+    }
+
+    /*This function return the MAC address of the client running the program*/
+    public static String getMacAddress() {
+        String result = "";
+        try {
+            for (NetworkInterface ni : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                byte[] address = ni.getHardwareAddress();
+                if (address != null) {
+                    for (int i = 0; i < address.length; i++)
+                        result += String.format((i == 0 ? "" : "-") + "%02X", address[i]);
+                    return result;
+                }
+            }
+        } catch (SocketException e) {
+            System.out.println("MAC Adress not found");
+            System.exit(1);
+        }
+        return result;
+    }
+}
---
+++ Test/src/Publisher.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import java.lang.*;
+
+public class Publisher extends Thread {
+  public static int port;
+  public static String ip;
+
+  /*Constructor for the Publisher*/
+  public Publisher(String Ip, int Port) {
+    ip = Ip;
+    port = Port;
+  }
+
+  public static void main(String[] args) throws IOException {
+    String[] splitCommand; //Array used to split the string command
+    String topic; //The topic that the user wrote
+    String message; //The data that the user wrote
+    Scanner sc = new Scanner(System.in);
+    Socket socket = null; //Socket which make the connection with the server
+    try {
+      System.out.println("Enter the port (Ex: 9999)");
+      String scanPort = sc.nextLine();//Try to take the port
+      port = Integer.parseInt(scanPort);//Parse the port to have an int
+    } catch (Exception e) {
+      System.out.println("Invalid port"); //If the user doesn't write a number
+    }
+    while (true) {
+      System.out.println("Publish [ServerIp] [Topic] [Data] or Exit");
+      String command = sc.nextLine().toLowerCase(); //Scan the publish command
+      splitCommand = command.split(" "); //Split the command
+      //The command should have 1 or 4 arguments
+      if (splitCommand.length != 1 && splitCommand.length != 4) {
+        System.out.println("Invalid command");
+      } else {
+        if (splitCommand[0].equals("exit")) {
+          break; //stop the loop
+        } else if (splitCommand[0].equals("publish")) {
+          ip = splitCommand[1];
+          topic = splitCommand[2];
+          message = splitCommand[3];
+          try {
+            socket = new Socket(ip, port); //Try to connect with the server
+            System.out.println("Successfully connected to broker" + socket.getRemoteSocketAddress().toString());
+            //Open the output stream and send the command
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            output.writeUTF("publish " + topic + " " + message);
+            System.out.println("Successfully send message to broker");
+
+          } catch (Exception e) {
+            System.out.println("Can't connect to broker " + e.getMessage());
+          }
+        } else {
+          System.out.println("Invalid command format.");
+        }
+      }
+    }
+    if (socket != null) //If the socket is not null we close it
+      socket.close();
+  }
+}
